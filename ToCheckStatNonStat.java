d3d0873e1e18445ae6c476ceb288e1859b97a4d9
shweta <junnarkarshweta3@gmail.com>
2023-08-25 18:34:10 +0530

    void (static non static method)

---
+++ ToCheckStatNonStat.java
@@ -0,0 +1,15 @@
+public class ToCheckStatNonStat {
+
+        static void method1()
+        {
+            System.out.println("this is void static");
+        }
+
+
+        void method2()
+        {
+            System.out.println("this is non static void method");
+        }
+
+
+}
